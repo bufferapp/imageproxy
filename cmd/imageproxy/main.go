35c38c0f9f366be0b4d968da92175fad3e0cd585
Karel McGrail <karel@bufferapp.com>
2020-03-23 20:44:47 +0000

    Removing the nil parameter to prevent defaultMuxServe settings applying

--- cmd/imageproxy/main.go
+++ cmd/imageproxy/main.go
@@ -124,7 +124,7 @@ func main() {
 	fmt.Printf("imageproxy listening on %s\n", server.Addr)
 
 	http.Handle("/", handler)
-	log.Fatal(http.ListenAndServe())
+	log.Fatal(http.ListenAndServe(*addr))
 }
 
 // tieredCache allows specifying multiple caches via flags, which will create
